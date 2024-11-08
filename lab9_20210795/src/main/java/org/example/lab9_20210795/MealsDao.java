abbb3f56ffd02de984ac6b59c57d3caef210e759
FabricioEstrada <a20210795@pucp.edu.pe>
2024-11-08 16:32:30 -0500

    ya se termino los detalles de comida

--- lab9_20210795/src/main/java/org/example/lab9_20210795/MealsDao.java
+++ lab9_20210795/src/main/java/org/example/lab9_20210795/MealsDao.java
@@ -2,6 +2,7 @@ package org.example.lab9_20210795;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -9,21 +10,41 @@ import java.util.Arrays;
 import java.util.List;
 @Component
 public class MealsDao {
-    public List<Meals> listar() {
+    public List<Meals> listar(String searchMeal) {
 
         List<Meals> lista = new ArrayList<>();
 
         RestTemplate restTemplate = new RestTemplate();
 
-        String endPoint = "http://localhost:8080/product";
+        String endPoint = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searchMeal;
 
-        ResponseEntity<Meals[]> responseEntity = restTemplate.getForEntity(endPoint, Meals[].class);
+        ResponseEntity<MealsResponse> responseEntity = restTemplate.getForEntity(endPoint, MealsResponse.class);
 
         if(responseEntity.getStatusCode().is2xxSuccessful()){
-            Meals[] body = responseEntity.getBody();
-            lista = Arrays.asList(body);
+            MealsResponse body = responseEntity.getBody();
+            if (body != null && body.getMeals() != null) {
+                lista = body.getMeals(); // Obtener la lista de meals
+            }
         }
 
         return lista;
+    }
+    public Meals detallesID(String ID) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // este link se ha usado de la propia IP
+        String endPoint = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + ID;
+
+        ResponseEntity<MealsResponse> responseEntity = restTemplate.getForEntity(endPoint, MealsResponse.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            MealsResponse body = responseEntity.getBody();
+            if (body != null && body.getMeals() != null && !body.getMeals().isEmpty()) {
+                // Devuelve el primer (y único) elemento de la lista
+                return body.getMeals().get(0);
+            }
+        }
+        //si no no devuelve nada
+        return null;
     }
 }
